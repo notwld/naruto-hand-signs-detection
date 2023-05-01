55a519760215e833eda580e0c1b97281a68bcf71
notwld <mwfarrukh@gmail.com>
2023-05-01 15:43:20 +0500

    data loaded

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # To check if GPU is active
 from tensorflow.python.client import device_lib
 
+import random
+
 # Load Data
 import os
 import cv2
@@ -21,3 +23,70 @@ print(device_lib.list_local_devices())
 
 train_dir = r'train'
 test_dir = r'test'
+
+def center_crop(img):
+    """Returns center cropped image
+    Args:
+    img: image to be center cropped
+    dim: dimensions (width, height) to be cropped
+    """
+    width, height = img.shape[1], img.shape[0]
+
+    # process crop width and height for max available dimension
+    crop_width = crop_height = width if width <= height else height
+    mid_x, mid_y = int(width/2), int(height/2)
+    cw2, ch2 = int(crop_width/2), int(crop_height/2) 
+    crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
+    return crop_img
+
+
+def scale_image(img, factor=1):
+    """Returns resize image by scale factor.
+    This helps to retain resolution ratio while resizing.
+    Args:
+    img: image to be scaled
+    factor: scale factor to resize
+    """
+    return cv2.resize(img,(int(img.shape[1]*factor), int(img.shape[0]*factor)))
+
+def rotate_image(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
+
+
+def get_data(data_dir) :
+    images = []
+    labels = []
+    
+    dir_list = os.listdir(data_dir)
+    for i in range(len(dir_list)):
+        print("Obtaining images of", dir_list[i], "...")
+        for image in os.listdir(data_dir + "/" + dir_list[i]):
+            img = cv2.imread(data_dir + '/' + dir_list[i] + '/' + image)
+            for rep in range(3):
+                clone = img.copy()
+                clone = rotate_image(clone, random.randint(5,10))
+                clone = scale_image(clone, 1 + random.randint(30, 600) / 100)
+                clone = center_crop(clone)
+                clone = cv2.resize(clone, (128, 128))
+                images.append(clone)
+                labels.append(i)
+            
+            img = cv2.resize(img, (128, 128))
+            images.append(img)
+            labels.append(i)
+            
+            
+    
+    return images, labels
+        
+X, y = get_data(train_dir)
+
+print("Data Loaded")
+
+print(len(X), len(y))
+
+classes = ['bird', 'boar', 'dog', 'dragon', 'hare', 'horse', 'monkey', 'ox', 'ram', 'rat', 'snake', 
+           'tiger', 'zero']
